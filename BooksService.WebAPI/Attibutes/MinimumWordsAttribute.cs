b0a6158e2d61b97a5f903915ddd6b45bdcf88e49
Egil Mijail Ordóñez Mazariegos <egilmijail@hotmail.com>
2025-07-31 19:34:22 -0600

    New rules

---
+++ BooksService.WebAPI/Attibutes/MinimumWordsAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksService.WebAPI.CustomAttributes
+{
+    public class MinimumWordsAttribute : ValidationAttribute
+    {
+        private readonly int _minWords;
+
+        public MinimumWordsAttribute(int minWords)
+        {
+            _minWords = minWords;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is string str && !string.IsNullOrEmpty(str))
+            {
+                string[] words = str.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (words.Length < _minWords)
+                {
+                    return new ValidationResult($"The {validationContext.DisplayName} must contain at least {_minWords} words.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
--- BooksService.WebAPI/Controllers/BooksController.cs
+++ BooksService.WebAPI/Controllers/BooksController.cs
@@ -6,13 +6,21 @@ namespace BooksService.WebAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-
     public class BooksController : ControllerBase
     {
         [HttpPost]
-        public async Task<IActionResult> Validate(Book book)
+        public async Task<IActionResult> Validate([FromBody] Book book)
         {
-            return Ok();
+            // La validación se activa automáticamente gracias a los atributos en el modelo Book.
+            // Si el modelo es inválido, ModelState.IsValid será falso.
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Simulación de una operación exitosa, ya que no se requiere base de datos.
+            // En una aplicación real, aquí se guardaría el libro en la base de datos.
+            return Ok(book);
         }
     }
 }
